a25895f5dc39f9055722fc9ecd7e22220979e58a
albertmvhr <alberts2027@student.cis.edu.hk>
2024-09-09 09:42:11 +0800

    ide formative done

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,25 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Albert
 *****************************************************************************************************
 */
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favouriteFood;
+        System.out.println("Please enter your age");
+        age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Please enter your first name");
+        firstName = scanner.nextLine();
+        System.out.println("Please enter your favourite food");
+        favouriteFood = scanner.nextLine();
+        System.out.println("First Name: " + firstName + "\n" + "Age: " + age + "\n" + "Favourite food: " + favouriteFood);
 
 
 
